d249ebcd3477eb97e2d853a716c6d7916bca681f
ether-nick <gabrieljohn.mendoza@yahoo.com>
2022-05-31 21:02:44 +0800

    basic stuffs

---
+++ Activity-3and4/Form1.Designer.vb
@@ -0,0 +1,281 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ViewBox = New System.Windows.Forms.TextBox()
+        Me.BtnClear = New System.Windows.Forms.Button()
+        Me.BtnExponent = New System.Windows.Forms.Button()
+        Me.BtnModulo = New System.Windows.Forms.Button()
+        Me.BtnDivide = New System.Windows.Forms.Button()
+        Me.BtnMultiply = New System.Windows.Forms.Button()
+        Me.BtnNine = New System.Windows.Forms.Button()
+        Me.BtnEight = New System.Windows.Forms.Button()
+        Me.BtnSeven = New System.Windows.Forms.Button()
+        Me.BtnSubtract = New System.Windows.Forms.Button()
+        Me.BtnSix = New System.Windows.Forms.Button()
+        Me.BtnFive = New System.Windows.Forms.Button()
+        Me.BtnFour = New System.Windows.Forms.Button()
+        Me.BtnAdd = New System.Windows.Forms.Button()
+        Me.BtnThree = New System.Windows.Forms.Button()
+        Me.BtnTwo = New System.Windows.Forms.Button()
+        Me.BtnOne = New System.Windows.Forms.Button()
+        Me.BtnEqual = New System.Windows.Forms.Button()
+        Me.BtnZero = New System.Windows.Forms.Button()
+        Me.BtnDecimal = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ViewBox
+        '
+        Me.ViewBox.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point)
+        Me.ViewBox.Location = New System.Drawing.Point(12, 12)
+        Me.ViewBox.Name = "ViewBox"
+        Me.ViewBox.ReadOnly = True
+        Me.ViewBox.Size = New System.Drawing.Size(330, 39)
+        Me.ViewBox.TabIndex = 0
+        Me.ViewBox.Text = "0"
+        Me.ViewBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'BtnClear
+        '
+        Me.BtnClear.Location = New System.Drawing.Point(12, 57)
+        Me.BtnClear.Name = "BtnClear"
+        Me.BtnClear.Size = New System.Drawing.Size(78, 78)
+        Me.BtnClear.TabIndex = 1
+        Me.BtnClear.Text = "C"
+        Me.BtnClear.UseVisualStyleBackColor = True
+        '
+        'BtnExponent
+        '
+        Me.BtnExponent.Location = New System.Drawing.Point(96, 57)
+        Me.BtnExponent.Name = "BtnExponent"
+        Me.BtnExponent.Size = New System.Drawing.Size(78, 78)
+        Me.BtnExponent.TabIndex = 2
+        Me.BtnExponent.Text = "xⁿ"
+        Me.BtnExponent.UseVisualStyleBackColor = True
+        '
+        'BtnModulo
+        '
+        Me.BtnModulo.Location = New System.Drawing.Point(180, 57)
+        Me.BtnModulo.Name = "BtnModulo"
+        Me.BtnModulo.Size = New System.Drawing.Size(78, 78)
+        Me.BtnModulo.TabIndex = 3
+        Me.BtnModulo.Text = "%"
+        Me.BtnModulo.UseVisualStyleBackColor = True
+        '
+        'BtnDivide
+        '
+        Me.BtnDivide.Location = New System.Drawing.Point(264, 57)
+        Me.BtnDivide.Name = "BtnDivide"
+        Me.BtnDivide.Size = New System.Drawing.Size(78, 78)
+        Me.BtnDivide.TabIndex = 4
+        Me.BtnDivide.Text = "/"
+        Me.BtnDivide.UseVisualStyleBackColor = True
+        '
+        'BtnMultiply
+        '
+        Me.BtnMultiply.Location = New System.Drawing.Point(264, 141)
+        Me.BtnMultiply.Name = "BtnMultiply"
+        Me.BtnMultiply.Size = New System.Drawing.Size(78, 78)
+        Me.BtnMultiply.TabIndex = 8
+        Me.BtnMultiply.Text = "*"
+        Me.BtnMultiply.UseVisualStyleBackColor = True
+        '
+        'BtnNine
+        '
+        Me.BtnNine.Location = New System.Drawing.Point(180, 141)
+        Me.BtnNine.Name = "BtnNine"
+        Me.BtnNine.Size = New System.Drawing.Size(78, 78)
+        Me.BtnNine.TabIndex = 7
+        Me.BtnNine.Text = "9"
+        Me.BtnNine.UseVisualStyleBackColor = True
+        '
+        'BtnEight
+        '
+        Me.BtnEight.Location = New System.Drawing.Point(96, 141)
+        Me.BtnEight.Name = "BtnEight"
+        Me.BtnEight.Size = New System.Drawing.Size(78, 78)
+        Me.BtnEight.TabIndex = 6
+        Me.BtnEight.Text = "8"
+        Me.BtnEight.UseVisualStyleBackColor = True
+        '
+        'BtnSeven
+        '
+        Me.BtnSeven.Location = New System.Drawing.Point(12, 141)
+        Me.BtnSeven.Name = "BtnSeven"
+        Me.BtnSeven.Size = New System.Drawing.Size(78, 78)
+        Me.BtnSeven.TabIndex = 5
+        Me.BtnSeven.Text = "7"
+        Me.BtnSeven.UseVisualStyleBackColor = True
+        '
+        'BtnSubtract
+        '
+        Me.BtnSubtract.Location = New System.Drawing.Point(264, 225)
+        Me.BtnSubtract.Name = "BtnSubtract"
+        Me.BtnSubtract.Size = New System.Drawing.Size(78, 78)
+        Me.BtnSubtract.TabIndex = 12
+        Me.BtnSubtract.Text = "-"
+        Me.BtnSubtract.UseVisualStyleBackColor = True
+        '
+        'BtnSix
+        '
+        Me.BtnSix.Location = New System.Drawing.Point(180, 225)
+        Me.BtnSix.Name = "BtnSix"
+        Me.BtnSix.Size = New System.Drawing.Size(78, 78)
+        Me.BtnSix.TabIndex = 11
+        Me.BtnSix.Text = "6"
+        Me.BtnSix.UseVisualStyleBackColor = True
+        '
+        'BtnFive
+        '
+        Me.BtnFive.Location = New System.Drawing.Point(96, 225)
+        Me.BtnFive.Name = "BtnFive"
+        Me.BtnFive.Size = New System.Drawing.Size(78, 78)
+        Me.BtnFive.TabIndex = 10
+        Me.BtnFive.Text = "5"
+        Me.BtnFive.UseVisualStyleBackColor = True
+        '
+        'BtnFour
+        '
+        Me.BtnFour.Location = New System.Drawing.Point(12, 225)
+        Me.BtnFour.Name = "BtnFour"
+        Me.BtnFour.Size = New System.Drawing.Size(78, 78)
+        Me.BtnFour.TabIndex = 9
+        Me.BtnFour.Text = "4"
+        Me.BtnFour.UseVisualStyleBackColor = True
+        '
+        'BtnAdd
+        '
+        Me.BtnAdd.Location = New System.Drawing.Point(264, 309)
+        Me.BtnAdd.Name = "BtnAdd"
+        Me.BtnAdd.Size = New System.Drawing.Size(78, 78)
+        Me.BtnAdd.TabIndex = 16
+        Me.BtnAdd.Text = "+"
+        Me.BtnAdd.UseVisualStyleBackColor = True
+        '
+        'BtnThree
+        '
+        Me.BtnThree.Location = New System.Drawing.Point(180, 309)
+        Me.BtnThree.Name = "BtnThree"
+        Me.BtnThree.Size = New System.Drawing.Size(78, 78)
+        Me.BtnThree.TabIndex = 15
+        Me.BtnThree.Text = "3"
+        Me.BtnThree.UseVisualStyleBackColor = True
+        '
+        'BtnTwo
+        '
+        Me.BtnTwo.Location = New System.Drawing.Point(96, 309)
+        Me.BtnTwo.Name = "BtnTwo"
+        Me.BtnTwo.Size = New System.Drawing.Size(78, 78)
+        Me.BtnTwo.TabIndex = 14
+        Me.BtnTwo.Text = "2"
+        Me.BtnTwo.UseVisualStyleBackColor = True
+        '
+        'BtnOne
+        '
+        Me.BtnOne.Location = New System.Drawing.Point(12, 309)
+        Me.BtnOne.Name = "BtnOne"
+        Me.BtnOne.Size = New System.Drawing.Size(78, 78)
+        Me.BtnOne.TabIndex = 13
+        Me.BtnOne.Text = "1"
+        Me.BtnOne.UseVisualStyleBackColor = True
+        '
+        'BtnEqual
+        '
+        Me.BtnEqual.Location = New System.Drawing.Point(180, 393)
+        Me.BtnEqual.Name = "BtnEqual"
+        Me.BtnEqual.Size = New System.Drawing.Size(163, 78)
+        Me.BtnEqual.TabIndex = 19
+        Me.BtnEqual.Text = "="
+        Me.BtnEqual.UseVisualStyleBackColor = True
+        '
+        'BtnZero
+        '
+        Me.BtnZero.Location = New System.Drawing.Point(96, 393)
+        Me.BtnZero.Name = "BtnZero"
+        Me.BtnZero.Size = New System.Drawing.Size(78, 78)
+        Me.BtnZero.TabIndex = 18
+        Me.BtnZero.Text = "0"
+        Me.BtnZero.UseVisualStyleBackColor = True
+        '
+        'BtnDecimal
+        '
+        Me.BtnDecimal.Location = New System.Drawing.Point(12, 393)
+        Me.BtnDecimal.Name = "BtnDecimal"
+        Me.BtnDecimal.Size = New System.Drawing.Size(78, 78)
+        Me.BtnDecimal.TabIndex = 17
+        Me.BtnDecimal.Text = "."
+        Me.BtnDecimal.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(355, 485)
+        Me.Controls.Add(Me.BtnEqual)
+        Me.Controls.Add(Me.BtnZero)
+        Me.Controls.Add(Me.BtnDecimal)
+        Me.Controls.Add(Me.BtnAdd)
+        Me.Controls.Add(Me.BtnThree)
+        Me.Controls.Add(Me.BtnTwo)
+        Me.Controls.Add(Me.BtnOne)
+        Me.Controls.Add(Me.BtnSubtract)
+        Me.Controls.Add(Me.BtnSix)
+        Me.Controls.Add(Me.BtnFive)
+        Me.Controls.Add(Me.BtnFour)
+        Me.Controls.Add(Me.BtnMultiply)
+        Me.Controls.Add(Me.BtnNine)
+        Me.Controls.Add(Me.BtnEight)
+        Me.Controls.Add(Me.BtnSeven)
+        Me.Controls.Add(Me.BtnDivide)
+        Me.Controls.Add(Me.BtnModulo)
+        Me.Controls.Add(Me.BtnExponent)
+        Me.Controls.Add(Me.BtnClear)
+        Me.Controls.Add(Me.ViewBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ViewBox As TextBox
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents BtnExponent As Button
+    Friend WithEvents BtnModulo As Button
+    Friend WithEvents BtnDivide As Button
+    Friend WithEvents BtnMultiply As Button
+    Friend WithEvents BtnNine As Button
+    Friend WithEvents BtnEight As Button
+    Friend WithEvents BtnSeven As Button
+    Friend WithEvents BtnSubtract As Button
+    Friend WithEvents BtnSix As Button
+    Friend WithEvents BtnFive As Button
+    Friend WithEvents BtnFour As Button
+    Friend WithEvents BtnAdd As Button
+    Friend WithEvents BtnThree As Button
+    Friend WithEvents BtnTwo As Button
+    Friend WithEvents BtnOne As Button
+    Friend WithEvents BtnEqual As Button
+    Friend WithEvents BtnZero As Button
+    Friend WithEvents BtnDecimal As Button
+End Class
---
+++ Activity-3and4/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Dim InitialValue As Double
+    Dim SelectedOperator As String
+
+    Private Sub OnNumberClick(btn As Button, e As EventArgs) Handles BtnZero.Click, BtnOne.Click, BtnTwo.Click, BtnThree.Click, BtnFour.Click, BtnFive.Click, BtnSix.Click, BtnSeven.Click, BtnEight.Click, BtnNine.Click, BtnDecimal.Click
+		If ViewBox.Text = "0" And btn Is BtnDecimal Then
+			ViewBox.Text = "0."
+		ElseIf ViewBox.Text = "0" Then
+			ViewBox.Text = btn.Text
+		Else
+			ViewBox.Text += btn.Text
+		End If
+		
+		If SelectedOperator = Nothing Then
+			InitialValue = Double.Parse(ViewBox.Text)
+		End If
+    End Sub
+	
+	Private Sub OnOperatorClick(opt As Button, e As EventArgs) Handles BtnAdd.Click, BtnSubtract.Click, BtnMultiply.Click, BtnDivide.Click, BtnModulo.Click, BtnExponent.Click, BtnClear.Click
+		If opt IsNot BtnClear Then
+			SelectedOperator = opt.Text
+		ElseIf opt Is BtnClear Then
+            InitialValue = Nothing
+			SelectedOperator = Nothing
+        End If
+		ViewBox.Text = "0"
+	End Sub
+	
+	Private Sub OnEqualClick(opt As Button, e As EventArgs) Handles BtnEqual.Click
+		If SelectedOperator = "+" Then
+			ViewBox.Text = (InitialValue + Double.Parse(ViewBox.Text))
+		ElseIf SelectedOperator = "-" Then
+			ViewBox.Text = (InitialValue - Double.Parse(ViewBox.Text))
+		ElseIf SelectedOperator = "*" Then
+			ViewBox.Text = (InitialValue * Double.Parse(ViewBox.Text))
+		ElseIf SelectedOperator = "/" Then
+			ViewBox.Text = (InitialValue / Double.Parse(ViewBox.Text))
+		ElseIf SelectedOperator = "%" Then
+			ViewBox.Text = (InitialValue Mod Double.Parse(ViewBox.Text))
+		ElseIf SelectedOperator = "xⁿ" Then
+			ViewBox.Text = (InitialValue ^ Double.Parse(ViewBox.Text))
+		End If
+	End Sub
+End Class
